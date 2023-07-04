0cef5b6c1f6835a0734427024372e0efb575393d
Sahil Gaurkar <sahilgaurkar@gmail.com>
2023-07-04 01:00:26 +0100

    Added Driver node launch

---
+++ src/webots_baxter/src/my_robot_driver.py
@@ -0,0 +1,30 @@
+import rclpy
+from geometry_msgs.msg import Twist
+
+HALF_DISTANCE_BETWEEN_WHEELS = 0.045
+WHEEL_RADIUS = 0.025
+
+class MyRobotDriver:
+    def init(self, webots_node, properties):
+        self.__robot = webots_node.robot
+
+        self.__left_motor = self.__robot.getDevice('right_s0')
+
+        self.__left_motor.setPosition(1)
+        self.__left_motor.setVelocity(0)
+
+        self.__target_twist = Twist()
+
+        rclpy.init(args=None)
+        self.__node = rclpy.create_node('my_robot_driver')
+        self.__node.create_subscription(Twist, 'cmd_vel', self.__cmd_vel_callback, 1)
+
+    def __cmd_vel_callback(self, twist):
+        self.__target_twist = twist
+
+    def step(self):
+        rclpy.spin_once(self.__node, timeout_sec=0)
+
+        command_motor_left = 1
+
+        self.__left_motor.setVelocity(command_motor_left)
