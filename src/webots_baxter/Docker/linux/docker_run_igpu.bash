d3b5fb49a74d6223472115d340d2f1ed2e98ae62
Sahil Gaurkar <sahilgaurkar@gmail.com>
2023-07-09 23:36:29 +0100

    Docker Run Files

---
+++ src/webots_baxter/Docker/linux/docker_run_igpu.bash
@@ -0,0 +1,26 @@
+# If not working, first do: sudo rm -rf /tmp/.docker.xauth
+# It still not working, try running the script as root.
+## Build the image first
+### docker build -t r2_path_planning .
+## then run this script
+
+
+xhost local:$USER
+
+
+XAUTH=/home/$USER/.Xauthority
+
+
+docker run --rm -it \
+    --name=baxter_test \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume /home/$USER/.Xauthority:/dot.Xauthority \
+    --net=host \
+    --privileged \
+    baxter:noetic \
+    bash
+
+echo "Done."
